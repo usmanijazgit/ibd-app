641502b896fb300ab20629c755f861ce4dec1f3d
Usman Ijaz <usman.ijaz@city.ac.uk>
2020-06-23 20:54:36 +0100

    React first commit

--- App.js
+++ App.js
@@ -1,19 +1,28 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View>
+      
+      <View>
+        <Text>Terms & Conditions</Text>
+        <Text>By using this application, you acknowledge 
+          that the content of this website is targeted 
+          at health care professionals only...</Text>
+        <Text>I am a healthcare professional and I accept the Terms of Use.</Text> 
+        <Button title="I ACCEPT" />
+      </View>
+
+      <View>
+
+
+      </View>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  
 });
